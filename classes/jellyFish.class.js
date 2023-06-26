18b72eaa4363e866a5c728c53d4e38ac7ffc4d26
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2023-06-26 07:48:59 +0200

    added enemies.class

--- classes/jellyFish.class.js
+++ classes/jellyFish.class.js
@@ -1,10 +1,7 @@
-class JellyFish extends MovableObject {
+class JellyFish extends Enemies {
 
 	constructor() {
 		super().loadImg('../img/2.Enemy/2 Jelly fish/Regular damage/Yellow 1.png');
-		this.y = 50 + Math.random() * 300;
-		this.x = 300 + Math.random() * 1600;
-		this.width = 70;
-		this.height = 100;
+		
 	}
 }
--- classes/pufferFish.class.js
+++ classes/pufferFish.class.js
@@ -1,10 +1,7 @@
-class PufferFish extends MovableObject {
+class PufferFish extends Enemies {
 
 	constructor() {
 		super().loadImg('../img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
-		this.y = 50 + Math.random() * 350;
-		this.x = 300 + Math.random() * 1500;
-		this.width = 120;
-		this.height = 70;
+
 	}
 }
