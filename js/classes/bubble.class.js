2b0e516bcbe536f1cc88b8e612d12f0a5040c6ac
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2023-07-02 15:15:43 +0200

    working on bubble2

--- js/classes/bubble.class.js
+++ js/classes/bubble.class.js
@@ -2,23 +2,24 @@ class Bubble extends AllBubbles{
 
 	width = 0;
 	height = 0;
+	bubbles = [];
 
-	bubbles = [
-		'../img/1.Sharkie/4.Attack/Bubble trap/Bubble.png'
-	]
+	// bubbles = [
+	// 	'../img/1.Sharkie/4.Attack/Bubble trap/Bubble.png'
+	// ]
 	
 
 	constructor(sharkie) {
 		super().loadImg('../img/1.Sharkie/4.Attack/Bubble trap/Bubble.png');
-		this.sharkie = sharkie
-		this.x = this.sharkie.x;
-		this.y = this.sharkie.y;
+		this.sharkie = sharkie;
 		this.animateBubble();
 	}
 
 	animateBubble() {
 		setInterval(() => {
 		if(this.sharkie.bubble) {
+			this.x = this.sharkie.x
+			this.y = this.sharkie.y
 			setInterval(() => {
 			this.gotIt = false;
 			this.width = 50;
