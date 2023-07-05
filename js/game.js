a247c76a17b68e2141b0bac7e769de99eee72b89
Martin Buder <132884932+martinBuder@users.noreply.github.com>
2023-07-05 12:53:15 +0200

    added on handyBtn2

--- js/game.js
+++ js/game.js
@@ -9,6 +9,80 @@ let sharkie;
 	*/
 function init() {
 	canvas = document.getElementById('canvas');
+	
+document.getElementById('btnLeft').addEventListener('touchstart', (e) => { e.preventDefault();
+	keyboard.LEFT = true;
+	});
+
+document.getElementById('btnLeft').addEventListener('touchend', (e) => { e.preventDefault();
+	keyboard.LEFT = false;
+});
+
+document.getElementById('btnRight').addEventListener('touchstart', (e) => { e.preventDefault();
+	keyboard.RIGHT = true;
+	});
+
+document.getElementById('btnRight').addEventListener('touchend', (e) => { e.preventDefault();
+	keyboard.RIGHT = false;
+});
+
+document.getElementById('btnUp1').addEventListener('touchstart', (e) => { e.preventDefault();
+	keyboard.UP = true;
+	});
+
+document.getElementById('btnUp1').addEventListener('touchend', (e) => { e.preventDefault();
+	keyboard.UP = false;
+});
+
+document.getElementById('btnUp2').addEventListener('touchstart', (e) => { e.preventDefault();
+	keyboard.UP = true;
+	});
+
+	
+document.getElementById('btnUp2').addEventListener('touchend', (e) => { e.preventDefault();
+	keyboard.UP = false;
+});
+
+document.getElementById('btnDown1').addEventListener('touchend', (e) => { e.preventDefault();
+	keyboard.DOWN = true;
+});
+
+	document.getElementById('btnDown1').addEventListener('touchend', (e) => { e.preventDefault();
+		keyboard.DOWN = false;
+	});
+
+	document.getElementById('btnDown2').addEventListener('touchstart', (e) => { e.preventDefault();
+		keyboard.DOWN = true;
+		});
+
+	document.getElementById('btnDown2').addEventListener('touchend', (e) => { e.preventDefault();
+		keyboard.DOWN = false;
+	});
+
+
+document.getElementById('btnBubble').addEventListener('touchstart', (e) => { e.preventDefault();
+	keyboard.BUBBLE = true;
+	});
+
+document.getElementById('btnBubble').addEventListener('touchend', (e) => { e.preventDefault();
+	keyboard.BUBBLE = false;
+});
+
+document.getElementById('btnPoisonbubble').addEventListener('touchstart', (e) => { e.preventDefault();
+	keyboard.POISONBUBBLE = true;
+	});
+
+document.getElementById('btnPoisonbubble').addEventListener('touchend', (e) => { e.preventDefault();
+	keyboard.POISONBUBBLE = false;
+});
+
+document.getElementById('btnSlap').addEventListener('touchstart', (e) => { e.preventDefault();
+	keyboard.SLAP = true;
+	});
+
+document.getElementById('btnSlap').addEventListener('touchend', (e) => { e.preventDefault();
+	keyboard.SLAP = false;
+});
 }
 
 /**
@@ -36,7 +110,6 @@ function muteBrowser() {
 	* definated keydowns 
  */
 window.addEventListener('keydown', (e) => {
-	console.log(e);
 	if(e.keyCode == 39) {
 		keyboard.RIGHT = true;
 	}
@@ -89,5 +162,5 @@ window.addEventListener('keyup', (e) => {
 });
 
 
-	
+
 
